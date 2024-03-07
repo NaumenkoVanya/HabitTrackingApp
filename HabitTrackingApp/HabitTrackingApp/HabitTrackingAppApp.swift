df070da1f378c8e8a10e40c74984324446790093
Vanya <whitewolf3310@icloud.com>
2024-03-07 11:29:37 +0300

    Добавил робочий проект из домашнего задания

--- HabitTrackingApp/HabitTrackingApp/HabitTrackingAppApp.swift
+++ HabitTrackingApp/HabitTrackingApp/HabitTrackingAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct HabitTrackingAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HabitTrackingAppTwo()
         }
     }
 }
